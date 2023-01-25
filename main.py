29a5fd97746de753c1328f2dce6aeefebfcb2b60
Abir Hasan <81650568+Abirsonet563@users.noreply.github.com>
2023-01-25 15:36:36 +0800

    new

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+import string
+import nameslist
+
+email_providers = ["yahoo", "gmail","hotmail", "outlook"]
+tlds = [".com",".co.uk", ".net", ".au",".org"]
+
+for i in range (1000):
+    random_numbers = random.randint(1,2)
+    random_server_sel = "@"+random.choice(email_providers)+random.choice(tlds)
+    randnames = random.choice(nameslist.Names)
+    random_2lettercomb = ''.join(random.choice(string.ascii_letters) for i in range (2))
+    print(randnames+random_2lettercomb+str(random_numbers)+random_server_sel)
+
+#don't forget to create another file (nameslist)
